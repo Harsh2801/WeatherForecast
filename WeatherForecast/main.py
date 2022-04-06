0c566c5ed97e416bb4ba38f2e14420ee774b4c04
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-04-06 13:36:54 +0530

    Add files via upload

---
+++ WeatherForecast/main.py
@@ -0,0 +1,8 @@
+from GandhiWeather.MainWindow import MainWindow
+from GandhiWeather.IntroWindow import WelcomeWindow
+
+welcome_window = WelcomeWindow()
+welcome_window.mainloop()
+
+main_win = MainWindow()
+main_win.mainloop()
