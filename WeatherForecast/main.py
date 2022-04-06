5acc15b36f782d100ed291553f73708757f6a810
Harsh2801 <55402521+Harsh2801@users.noreply.github.com>
2022-04-06 13:37:34 +0530

    Delete WeatherForecast directory

--- WeatherForecast/main.py
+++
@@ -1,8 +0,0 @@
-from GandhiWeather.MainWindow import MainWindow
-from GandhiWeather.IntroWindow import WelcomeWindow
-
-welcome_window = WelcomeWindow()
-welcome_window.mainloop()
-
-main_win = MainWindow()
-main_win.mainloop()
